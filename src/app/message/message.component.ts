880c4a2ffa4d065b05172465f5591237d3735c4f
Maksim Sergienko <creativesun@yandex.ru>
2024-07-03 20:56:22 +0300

    feat(message): add message component

---
+++ src/app/message/message.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+import { AvatarComponent } from '../avatar/avatar.component';
+import { NgClass, NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-message',
+  standalone: true,
+  imports: [AvatarComponent, NgClass, NgIf],
+  templateUrl: './message.component.html',
+  styleUrl: './message.component.scss'
+})
+export class MessageComponent {
+  @Input({required: true}) messageText = "";
+  @Input({required: true}) isMessageFromBot = true;
+
+  // constructor(){
+  //   if (this.isMessageFromBot) {
+  //     Attr.ba
+  //   }
+  // }
+}
