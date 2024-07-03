74c5886c0a3419a11976cbff2969ccac073a59b7
Maksim Sergienko <creativesun@yandex.ru>
2024-07-03 19:00:14 +0300

    feat(app): preload commands

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as data from '../messages.mock.json';
+
+interface ICommands {
+  commands: Array<{
+    id: number;
+    command: string;
+    response: string;
+  }>
+  unknownCommandResponse: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'rt-labs-test-chat-bot';
+  constructor() {
+    this.preloadMessages();
+  }
+  preloadMessages = async () => {
+    try {
+      let messagesData: ICommands = await data;
+      console.log('messagesData', messagesData);
+    } catch (e) {
+      console.error(e);
+    }
+  };
 }
