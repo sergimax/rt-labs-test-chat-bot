74a3f2634e42e3aaa00251b20fccb4662644c9c5
Maksim Sergienko <creativesun@yandex.ru>
2024-07-03 22:24:05 +0300

    feat(app): use chat-input component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import {AvatarComponent} from "./avatar/avatar.component"
 import {MessageComponent} from "./message/message.component"
 import {HeaderComponent} from "./header/header.component"
+import {ChatInputComponent} from "./chat-input/chat-input.component";
 import * as data from '../messages.mock.json';
 
 interface ICommands {
@@ -17,7 +18,7 @@ interface ICommands {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AvatarComponent, MessageComponent, HeaderComponent],
+  imports: [RouterOutlet, AvatarComponent, MessageComponent, HeaderComponent, ChatInputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
